d0cbfcf0ef00404fbc74ce661162f2da1100f234
LeeHsss <lii0730@icloud.com>
2022-03-30 09:53:25 +0900

    Initial Commit

---
+++ MVVM_Bank/MVVM_BankApp.swift
@@ -0,0 +1,17 @@
+//
+//  MVVM_BankApp.swift
+//  MVVM_Bank
+//
+//  Created by LeeHsss on 2022/03/30.
+//
+
+import SwiftUI
+
+@main
+struct MVVM_BankApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
